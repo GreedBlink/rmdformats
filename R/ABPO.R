f5cd2ca9eaed1dd8a041bc5343becd7992682846
Jonatha Azevedo <johnmasterchip@gmail.com>
2018-04-18 21:00:15 -0300

    Update and rename readthedown.R to ABPO.R

--- R/ABPO.R
+++ R/ABPO.R
@@ -29,7 +29,7 @@
 #' @export
 
 
-readthedown <- function(fig_width = 8,
+ABPO <- function(fig_width = 8,
                        fig_height = 5,
                        fig_caption = TRUE,
                        highlight = "kate",
